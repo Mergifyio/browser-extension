52232cf2ecedd773653a13a906da682db2ead9b4
Mehdi ABAAKOUK <sileht@sileht.net>
2025-03-26 16:27:58 +0100

    ci: add linters (#58)

--- src/sendInstallState.js
+++ src/sendInstallState.js
@@ -1,20 +1,21 @@
 function sendInstallState() {
-    window.postMessage({
-        mergifyExtensionVariant: "unknown-browser",
-        // For backward compatibility
-        isMergifyChromeExtensionInstalled: true,
-    }, '*');
+    window.postMessage(
+        {
+            mergifyExtensionVariant: "unknown-browser",
+            // For backward compatibility
+            isMergifyChromeExtensionInstalled: true,
+        },
+        "*",
+    );
 }
 
-(function() {
-    'use strict';
-
+(() => {
     const observer = new MutationObserver(() => {
-        if (location.href.includes('integrations')) {
+        if (location.href.includes("integrations")) {
             sendInstallState();
         }
     });
 
     const config = { subtree: true, childList: true };
     observer.observe(document.body, config);
-}());
+})();
