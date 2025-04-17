57cdde8b3a5a5994fe9817dcdd30232cd19448d9
Mehdi ABAAKOUK <sileht@sileht.net>
2025-04-17 15:40:18 +0200

    feat: add safari signing (#73)
    This rework the safari artefact to sign it with the CI.
    
    The signature for now is a Developer ID one, that allows distribution of
    the package outside app store.
    
    The process to publish it to the Apple App Store is a bit to tedious for
    us to support for now.
    
    Fixes: MRGFY-4982

---
+++ publish-safari-extension.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+APPLE_TEAM_ID=995U2925WN
+APPLE_CONNECT_ISSUER_ID=5f50b8cc-d123-4ae0-817f-33cf0d40f209
+APPLE_CONNECT_KEY_ID=24XPFWR3PC
+APPLE_CONNECT_KEY_PATH=private_keys/AuthKey_${APPLE_CONNECT_KEY_ID}.p8
+
+outfile=$1
+
+# Push the signed pkg to Apple
+xcrun notarytool submit $outfile \
+    --team-id "$APPLE_TEAM_ID" \
+    --key "$APPLE_CONNECT_KEY_PATH" \
+    --key-id "$APPLE_CONNECT_KEY_ID" \
+    --issuer "$APPLE_CONNECT_ISSUER_ID" \
+    --wait
+xcrun stapler staple $outfile
